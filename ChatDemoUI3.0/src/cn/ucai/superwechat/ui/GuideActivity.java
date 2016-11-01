a1c07394d5a9def0e05aa3faabae9afa22947b18
Long Tao <longtaojay@163.com>
2016-11-01 22:08:25 +0800

    添加butterknife和欢迎页面

--- ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
+++ ChatDemoUI3.0/src/cn/ucai/superwechat/ui/GuideActivity.java
@@ -1,9 +1,13 @@
 package cn.ucai.superwechat.ui;
 
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.ucai.superwechat.R;
+import cn.ucai.superwechat.utils.MFGT;
 
 public class GuideActivity extends BaseActivity {
 
@@ -12,5 +16,18 @@ public class GuideActivity extends BaseActivity {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_guide);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_login, R.id.btn_register})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_login:
+                MFGT.gotoLogin(this);
+                break;
+            case R.id.btn_register:
+                MFGT.gotoRegister(this);
+                break;
+        }
     }
 }
